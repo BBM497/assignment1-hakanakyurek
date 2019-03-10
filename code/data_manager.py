5deeba7799f97f1f7dc7985364d69a3b40a3c7e0
hakan <b21426553@cs.hacettepe.edu.tr>
2019-03-10 16:34:31 +0300

    label logic added to read.

--- code/data_manager.py
+++ code/data_manager.py
@@ -1,16 +1,26 @@
 import os
+import enum
+
+
+class Label(enum.Enum):
+    HAMILTON = 0
+    MADISON = 1
 
 
 def read(path=''):
     train_dataset = []
+    train_labels = []
     test_dataset = []
+    test_labels = []
 
     train_path = path + 'Train/'
     for file in os.listdir(train_path):
 
         file = open(train_path + file, 'r')
+        lines = file.readlines()
 
-        train_dataset.append(file.readlines())
+        train_labels.append(Label.HAMILTON if lines[0] == 'HAMILTON \n' else Label.MADISON)
+        train_dataset.append(lines[1])
 
         file.close()
 
@@ -18,10 +28,11 @@ def read(path=''):
     for file in os.listdir(test_path):
 
         file = open(test_path + file, 'r')
+        lines = file.readlines()
 
-        test_dataset.append(file.readlines())
+        test_labels.append(Label.HAMILTON if lines[0] == 'HAMILTON \n' else Label.MADISON)
+        test_dataset.append(lines[1])
 
         file.close()
 
     return train_dataset, test_dataset
-
